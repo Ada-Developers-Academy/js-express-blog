b19e2e5dfc1c31146b42bc0970c06156bc2fcfc0
Jeffrey Lembeck <jlembeck@gmail.com>
2015-02-27 10:15:54 -0800

    Bust out the post so we can implement

--- models/post.js
+++ models/post.js
@@ -2,19 +2,12 @@ var path = require( 'path' );
 var fs = require( 'fs' );
 
 var Post = function(opts){
-  this.id = opts.id;
-  this.date = new Date( opts.date );
-  this.title = opts.title;
-  this.body = opts.body;
 };
 
-Post.getAll = function(cb){
-  fs.readFile( path.join( __dirname, "..", "db", "posts.json" ), function( err, data ){
-    var postData = JSON.parse( data.toString( "utf-8" ) );
-    cb(postData.map(function( pd ){
-      return new Post( pd );
-    }));
-  });
+Post.all = function(cb){
+};
+
+Post.find = function(id, cb){
 };
 
 module.exports = Post;
--- test/post-test.js
+++ test/post-test.js
@@ -14,17 +14,37 @@ describe('Post', function () {
       };
       var p = new Post(options);
       assert.equal( p.id, 1, "Id should be 1" );
+      assert.equal( p.title, "Hi", "Title should match" );
+      assert.equal( p.body, "Howdy", "Body should match" );
       assert.equal( p.date.toString(), (new Date( options.date )).toString(), "Date should match a date object created with ISO date" );
     });
   });
 
-  describe( '#getAll', function(){
-    it('should get all Posts', function (done) {
-      Post.getAll(function( posts ){
+  describe( '#all', function(){
+    it('should get all Posts', function ( done ) {
+      var d = new Date("2015-02-27T04:19:05.293Z");
+      Post.all(function( err, posts ){
+        if( err ){
+          assert.ok( false, "Error thrown: " + err.stack );
+        }
         var p = posts[0];
         assert.equal( p.id, 1, "Id should be 1" );
+        assert.equal( p.title, "My first post", "Id should be 1" );
+        assert.equal( p.date.toString(), d.toString(), "Date should match first element" );
+        assert.equal( p.body, "This is my first post and I'm pretty excited", "Body should match" );
         done();
       });
     });
   });
+
+  describe( '#find', function(){
+    it( 'should find post by id', function( done ){
+      Post.find( 2, function( posts ){
+        var p = posts[0];
+        assert.equal( p.id, 2, "Id should be 2" );
+        done();
+      });
+    });
+  });
+
 });
